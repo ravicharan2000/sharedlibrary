86469837d224829ecacfd1dd88df2b92024b476a
ravicharan2000 <168518250+ravicharan2000@users.noreply.github.com>
2024-12-28 23:08:04 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -11,4 +11,9 @@ def buildArtifact()
 def tomcatDeploy(jobname,ip,context)
 {
     sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war"
-} 
+}
+
+def executeSelenium(jobname)
+{
+    sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+}
